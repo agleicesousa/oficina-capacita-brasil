66390093367a23643073a210ab12a4cd359bcf55
agleicesousa <aleicesousa98@gmail.com>
2025-01-16 20:59:24 -0300

    feat: função para sair da aplicação

--- src/pages/caixaEletronico/script.js
+++ src/pages/caixaEletronico/script.js
@@ -8,11 +8,19 @@ function login() {
     const password = document.getElementById("password").value;
     const loginMessage = document.getElementById("login-message");
 
-    if (password === "1234") { // Senha fixa para teste
+    if (password === "1234") {
         document.getElementById("login-screen").style.display = "none";
         document.getElementById("atm-screen").style.display = "block";
         loginMessage.textContent = "";
     } else {
         loginMessage.textContent = "Senha incorreta! Tente novamente.";
     }
+}
+
+function logout() {
+    document.getElementById("atm-screen").style.display = "none";
+    document.getElementById("transaction-history").style.display = "none";
+    document.getElementById("login-screen").style.display = "block";
+    document.getElementById("password").value = "";
+    document.getElementById("message").textContent = "";
 }
