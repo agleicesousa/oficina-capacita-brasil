153f63217060487297f59bf5d92464b550ae0db5
agleicesousa <aleicesousa98@gmail.com>
2025-01-16 19:30:44 -0300

    feat: const necessárias

--- src/pages/gerenciarTarefas/script.js
+++ src/pages/gerenciarTarefas/script.js
@@ -1,2 +1,9 @@
 // Atualiza o ano atual no rodapé
 document.getElementById('ano-atual').textContent = new Date().getFullYear();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const taskInput = document.getElementById('task-input');
+    const addTaskBtn = document.getElementById('add-task-btn');
+    const taskList = document.getElementById('task-list');
+    const yearSpan = document.getElementById('ano-atual');
+});
