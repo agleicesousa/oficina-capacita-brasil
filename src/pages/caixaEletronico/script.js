5f6447f09b9c88b8640d65dc3e61ebd6e3d54c30
agleicesousa <aleicesousa98@gmail.com>
2025-01-16 20:57:46 -0300

    feat: função para fazer login

--- src/pages/caixaEletronico/script.js
+++ src/pages/caixaEletronico/script.js
@@ -1,2 +1,18 @@
 // Atualiza o ano atual no rodapé
 document.getElementById('ano-atual').textContent = new Date().getFullYear();
+
+let balance = 0;
+let transactionHistory = [];
+
+function login() {
+    const password = document.getElementById("password").value;
+    const loginMessage = document.getElementById("login-message");
+
+    if (password === "1234") { // Senha fixa para teste
+        document.getElementById("login-screen").style.display = "none";
+        document.getElementById("atm-screen").style.display = "block";
+        loginMessage.textContent = "";
+    } else {
+        loginMessage.textContent = "Senha incorreta! Tente novamente.";
+    }
+}
